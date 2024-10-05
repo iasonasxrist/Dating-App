2c1f4efe99d8be056511042dc25817cb3f25db0e
iasonasxrist <iasonasxrist@gmail.com>
2024-10-05 19:29:30 +0300

    Fixes and configuration

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '**', redirectTo: '',  pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
